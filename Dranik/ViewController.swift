cd0f173ded0a6eb4e0c4c7db329addbed67fd431
Azarov Alex <alex@azarov.by>
2019-05-27 18:17:07 +0300

    New example

--- Dranik/ViewController.swift
+++ Dranik/ViewController.swift
@@ -10,30 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet private var label: UILabel!
-    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var originalObservableTextField: UITextField!
+    @IBOutlet private var filterExampleLabel: UILabel!
+    @IBOutlet private var mapExampleLabel: UILabel!
+    @IBOutlet private var mergeObservableTextField: UITextField!
+    @IBOutlet private var mergeExampleLabel: UILabel!
+    @IBOutlet private var flatMapExampleLabel: UILabel!
+
 
     let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let observable = Observable<String>.create { sink in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-                sink.emitValue("Hello")
-            })
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-                sink.emitValue("World")
-            })
-            DispatchQueue.main.asyncAfter(deadline: .now() + 4, execute: {
-                sink.emitValue("!!!!")
-            })
-        }
+        let observable = originalObservableTextField.textObservable()
 
-        observable.subscribe { [weak self] event in
-            guard let value = event.value else { return }
-            self?.label.text = value
-        }.disposed(by: bag)
+        observable
+            .filter { $0.count % 2 == 0}
+            .subscribe { [weak self] event in
+                guard let value = event.value else { return }
+                self?.filterExampleLabel.text = value
+            }.disposed(by: bag)
+
+        observable
+            .map { $0.count + 5 }
+            .subscribe { [weak self] event in
+                guard let value = event.value else { return }
+                self?.mapExampleLabel.text = String(value)
+            }.disposed(by: bag)
+
+        let secondObservable = mergeObservableTextField.textObservable()
+        Observable.merge(observables: observable, secondObservable)
+            .subscribe { [weak self] event in
+                guard let value = event.value else { return }
+                self?.mergeExampleLabel.text = value
+            }.disposed(by: bag)
+
+        observable
+            .flatMap { value -> Observable<String> in
+                let (sink, observable) = Observable<String>.pipe()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                    sink.emitValue(value)
+                })
+                return observable
+            }
+            .subscribe { [weak self] event in
+                guard let value = event.value else { return }
+                self?.flatMapExampleLabel.text = value
+            }.disposed(by: bag)
     }
 
     @IBAction func deinitViewController(_ sender: Any) {
