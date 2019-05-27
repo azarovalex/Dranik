20bac7107e2ba4fd450f4bf0a1c5129b2eb06fe9
Azarov Alex <alex@azarov.by>
2019-05-27 15:51:43 +0300

    Test target added

---
+++ DranikTests/DranikTests.swift
@@ -0,0 +1,18 @@
+//
+//  DranikTests.swift
+//  DranikTests
+//
+//  Created by Alex Azarov on 5/27/19.
+//  Copyright © 2019 Azarov Alex. All rights reserved.
+//
+
+import XCTest
+
+@testable import Dranik
+
+class DranikTests: XCTestCase {
+
+    private let disposeBag = DisposeBag()
+
+    
+}
